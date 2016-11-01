65e4c4b957fa60c6b7eddde522586ce419d580fa
raimonfuns <raimonfuns@163.com>
2016-11-01 18:21:49 +0800

    卫生间模块

--- ios_views/toiletPage.js
+++ ios_views/toiletPage.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import {
+  StyleSheet,
   Text,
   View
   } from 'react-native';
 
+import TWebView from './webview';
+
+const nearByURL = 'http://123.57.39.116:3000/html/nearby.html';
+//const nearByURL = './../html/nearby.html';
+
 class ToiletPage extends Component{
   render(){
-    return(
-      <View>
-        <Text>卫生间</Text>
-      </View>
-    );
+    return(<TWebView url={nearByURL} isNearBy={true}/>);
   }
 }
 
