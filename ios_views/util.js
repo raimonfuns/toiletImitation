591e398a7b58e637f5d33ab21b1309201987b621
raimonfuns <raimonfuns@163.com>
2016-11-01 17:59:43 +0800

    完成推荐主题列表

--- ios_views/util.js
+++ ios_views/util.js
@@ -4,13 +4,21 @@
  * 主要提供工具方法
  *
  */
+import Dimensions from 'Dimensions';
 import React, { Component } from 'react';
 import {
   PixelRatio
   } from 'react-native';
 
 module.exports = {
+  /*最小线宽*/
   pixel: 1 / PixelRatio.get(),
+
+  /*屏幕尺寸*/
+  size: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
 
   /**
    * 基于fetch的get方法
